3879f2dc1500345d5312cfd56e3b3b07df27dc05
Camille Martin <acamillemartin@gmail.com>
2025-10-23 05:52:18 +0800

    Initial commit: 100 days of code Python Project

---
+++ day_1_band_name_generator.py
@@ -0,0 +1,11 @@
+print("Welcome to the Band Name Generator.")
+
+# Ask for the city name
+city = input("What's the name of the city you grew up in?\n")
+
+# Ask for the pet's name
+pet = input("What's your pet's name?\n")
+
+# Generate and display the band name
+band_name = city + " " + pet
+print("Your band name could be " + band_name)
